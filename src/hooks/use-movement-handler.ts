f39b00b1b6b9431d404d2b097b09fbd024ca2212
Natalya <natalia_muratova@relex.ru>
2024-04-23 02:30:14 +0300

    Fix renders problems

--- src/hooks/use-movement-handler.ts
+++ src/hooks/use-movement-handler.ts
@@ -1,26 +1,30 @@
 import {useBoardSlides} from "./use-board-slides.ts";
 import {useCellsInitialization} from "./use-cells-initialization.ts";
 import {Direction} from "../types/direction.ts";
+import {useEffect, useState} from "react";
 
 export function useMovementHandler() {
   const { slideLeft, slideRight, slideUp, slideDown } = useBoardSlides();
   const { setValueInRandomPlace } = useCellsInitialization();
 
+  const [slideChanged, setSlideChanged] = useState(false);
+
   const makeSlidesMove = (direction: Direction) => {
     if (direction === 'left') {
       slideLeft();
-      setValueInRandomPlace();
     } else if (direction === 'right') {
       slideRight();
-      setValueInRandomPlace();
     } else if (direction === 'up') {
       slideUp();
-      setValueInRandomPlace();
     } else if (direction === 'down') {
       slideDown();
-      setValueInRandomPlace();
     }
+    setSlideChanged((prev) => !prev);
   }
+
+  useEffect(() => {
+    setValueInRandomPlace();
+  }, [slideChanged]);
 
   return {
     makeSlidesMove,
